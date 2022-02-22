e6fbfd7fa46b72e4fa75f537ff3e435127e0dbc8
Fernando Silva <fdograph@gmail.com>
2022-02-22 17:12:23 +0100

    Adding CNAME

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: {
