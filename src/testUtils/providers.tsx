93f5499da2481c0c8912a3c447c00a7236583563
Fernando Silva <fdograph@gmail.com>
2022-02-28 19:52:37 +0100

    mocks

--- src/testUtils/providers.tsx
+++ src/testUtils/providers.tsx
@@ -3,6 +3,13 @@ import { I18nextProvider, I18nextProviderProps } from 'react-i18next';
 import i18nInstance from './mockI18n';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+const mockI18nInstance = (mock: Partial<I18nextProviderProps['i18n']>) => ({
+  ...i18nInstance,
+  on: jest.fn(),
+  off: jest.fn(),
+  ...mock,
+});
+
 export const withI18n = (
   children: React.ReactNode,
   mock?: Partial<I18nextProviderProps['i18n']>
@@ -10,12 +17,7 @@ export const withI18n = (
   let mockI18n = i18nInstance;
 
   if (mock !== undefined) {
-    mockI18n = {
-      ...i18nInstance,
-      on: jest.fn(),
-      off: jest.fn(),
-      ...mock,
-    };
+    mockI18n = mockI18nInstance(mock);
   }
 
   return <I18nextProvider i18n={mockI18n}>{children}</I18nextProvider>;
